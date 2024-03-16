7b713599ce816080b28888f0a94d05a7d1443efe
giadyensukwabi <thurgid@gmail.com>
2024-03-16 13:03:35 +0000

    changed footer  text to uppercase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer () {
     return (
         <div className="w-full h-12 md:h-32 bg-black bottom-0 flex justify-center items-center">
-            <p className="text-white text-center">Colour Picker | 2024 </p>
+            <p className="text-white text-center">COLOR PICKER | 2024 </p>
         </div>
     )
 }
